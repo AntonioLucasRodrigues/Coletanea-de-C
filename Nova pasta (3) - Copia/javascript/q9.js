59af1a6976bbd019970fb3e75ae828bc2c100fff
Antonio Lucas <126987741+AntonioLucasRodrigues@users.noreply.github.com>
2026-03-15 17:42:11 -0400

    Delete Nova pasta (3) - Copia directory

--- Nova pasta (3) - Copia/javascript/q9.js
+++
@@ -1,8 +0,0 @@
-function BotaoClicado(){
-    let button = document.getElementById("button");
-    botao.innerHTML = "CLICADO :)";
-    botao.style.background ="yellow";
-    botao.style.color = "red";  
-}
-
-let botao1 = document.getElementById("button").addEventListener("click", BotaoClique);
